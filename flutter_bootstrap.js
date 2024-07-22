81c1ae7f665d8f74b71b1a41e5cb0549f37e6a7a
Vintesys | Aplicativos Personalizados <vintesys@gmail.com>
2024-07-22 19:53:37 -0300

    implementação do lançamento de parcelas de eventos sociais

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3522595253"
+    serviceWorkerVersion: "1455495556"
   }
 });
