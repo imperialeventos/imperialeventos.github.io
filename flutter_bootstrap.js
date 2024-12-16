219e6f98951e428941fa3641fe8ec91d70efd42a
Vintesys | Aplicativos Personalizados <vintesys@gmail.com>
2024-12-16 12:08:53 -0300

    Novas funcionalidades de acervo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2936651337"
+    serviceWorkerVersion: "2783072126"
   }
 });
